5bf90eccc3d42f2d1a496390be9641e82536a9e6
Rohit Chaudhary <rohit.chaudhary@lowes.com>
2024-10-12 23:38:39 +0545

    added method printLengthOfEachString in a list

--- streams/Lists.java
+++ streams/Lists.java
@@ -14,6 +14,11 @@ public class Lists {
         nums.stream().forEach(num -> System.out.println(num));
     }
 
+    private static void printLengthOfEachString(List<String> list) {
+        System.out.println("Printing length of each string in the list");
+        list.stream().map(str -> str.length()).forEach(System.out:: println);
+    }
+
     private static void print(int val) {
         System.out.println(val);
     }
@@ -22,6 +27,9 @@ public class Lists {
         List<Integer> list = Arrays.asList(1,2,3,4,5);
         printSumOfList(list);
         printList(list);
+
+        List<String> strList = Arrays.asList("MySQL", "Oracle", "Hbase", "Redis", "ElasticSearch", "Couchbase", "Cassandra");
+        printLengthOfEachString(strList);
     }
 
 }
