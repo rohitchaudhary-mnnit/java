e72775d40524836530c57b55d1b9eaa7c5f5471f
Rohit Chaudhary <rohit.chaudhary@lowes.com>
2024-10-13 13:52:18 +0545

    printLengthOfEachString and printSumOfLengthOfEachString using Array streams

--- streams/Array.java
+++ streams/Array.java
@@ -18,10 +18,25 @@ public class Array {
         Arrays.stream(nums).forEach(num -> System.out.println(num));
     }
 
+    private static void printLengthOfEachString(String[] arr) {
+        System.out.println("Printing length of each string in the array");
+        Arrays.stream(arr).forEach(s->System.out.println(s.length()));
+    }
+
+    private static void printSumOfLengthOfEachString(String[] arr) {
+        System.out.println("Printing sum of length of each string in the array");
+        int sum = Arrays.stream(arr).mapToInt(String::length).sum();
+        System.out.println(sum);
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[] {1, 2, 3, 4, 5};
         printSumOfArray(nums);
 
         printArray(nums);
+
+        String[] arr = new String[]{"MySQL", "Oracle", "Hbase", "Redis", "ElasticSearch", "Couchbase", "Cassandra"};
+        printLengthOfEachString(arr);
+        printSumOfLengthOfEachString(arr);
     }
 }
