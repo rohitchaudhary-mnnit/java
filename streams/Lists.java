bedd00ad7764b5dfd5453cc43850fd0656524a84
rohitchaudhary <chaudharyrohit264@gmail.com>
2025-10-20 18:15:29 +0530

    Added sum of list with skip logic method and optimised print list method

--- streams/Lists.java
+++ streams/Lists.java
@@ -6,12 +6,20 @@ public class Lists {
     private static void printSumOfList(List<Integer> nums){
         System.out.println("printing sum of list elements");
         int sum = nums.stream().mapToInt(Integer::intValue).sum();
+        //print sumOf elements skipping 2 integers from first
+        int skipSum = nums.stream().mapToInt(Integer::intValue).skip(2).sum();
         print(sum);
+        print(skipSum);
     }
 
     private static void printList(List<Integer> nums) {
         System.out.println("printing list elements");
         nums.stream().forEach(num -> System.out.println(num));
+    }
+
+    private static void printListOptimized(List<Integer> nums) {
+        System.out.println("printing list of elements in optimised way.");
+        nums.forEach(System.out::println);
     }
 
     private static void printLengthOfEachString(List<String> list) {
@@ -33,6 +41,7 @@ public class Lists {
         List<Integer> list = Arrays.asList(1,2,3,4,5);
         printSumOfList(list);
         printList(list);
+        printListOptimized(list);
 
         List<String> strList = Arrays.asList("MySQL", "Oracle", "Hbase", "Redis", "ElasticSearch", "Couchbase", "Cassandra");
         printLengthOfEachString(strList);
